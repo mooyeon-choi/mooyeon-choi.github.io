98d1eb027fee585fcc5f67576044b585dd96db92
mooyeon-choi <dus1208@ajou.ac.kr>
2020-11-24 00:00:30 +0900

    Add images

---
+++ myroom/src/app.jsx
@@ -0,0 +1,12 @@
+import './app.css';
+import mainpage from './images/mainpage.png';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={mainpage} />
+    </div>
+  );
+}
+
+export default App;
