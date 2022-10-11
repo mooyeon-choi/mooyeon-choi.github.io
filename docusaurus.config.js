4d914c564455ab1c542d56349e4eafc1160cc737
mooyeon-choi <lon12080@gmail.com>
2022-10-11 23:20:11 +0900

    Test Settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,6 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'mooyeon-choi', // Usually your GitHub org/user name.
   projectName: 'mooyeon-choi.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
