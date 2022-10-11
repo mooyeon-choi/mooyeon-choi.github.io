25fce3f46328608b09437e96b9e7c97057a8582b
mooyeon-choi <lon12080@gmail.com>
2022-10-11 23:47:31 +0900

    Test 변경 확인

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'TEST',
   tagline: 'Dinosaurs are cool',
   url: 'https://mooyeon-choi.github.io',
   baseUrl: '/',
