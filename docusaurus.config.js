d73bc0423a682cd14f415c1b117d8657fd32838f
mooyeon-choi <lon12080@gmail.com>
2022-10-12 23:04:41 +0900

    baseUrl 원복

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Mylog',
-  url: 'https://mylog.site',
+  url: 'https://mooyeon-choi.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
