06188ff96e78fa95cb17ac6cacf7f659bebc5cf3
mooyeon-choi <lon12080@naver.com>
2024-07-07 22:05:24 +0900

    feat: add pacman

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ export default function Home(): JSX.Element {
 
   const [animate, setAnimate] = useState(false);
   const [textPopUp, setTextPopUp] = useState(false);
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     setTimeout(() => {
@@ -20,6 +21,19 @@ export default function Home(): JSX.Element {
     setTimeout(() => {
       setTextPopUp(true);
     }, 5500);
+
+    
+    const interval = setInterval(() => {
+      setCount((prevCount) => {
+        if (prevCount >= 150) {
+          clearInterval(interval);
+          return 0;
+        }
+        return prevCount + 1;
+      });
+    }, 100);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
@@ -30,13 +44,16 @@ export default function Home(): JSX.Element {
         {() => {
           return (
             <div id='container'>
-              <div id='text-object'>
+              <div id='text-object' className={count > 83 ? 'wipe-out' : 'initial'}>
                 MOOYEON
               </div>
-              <div id='dot-object' className={animate ? 'animate' : ''}></div>
-              <div id='text-swing-first' className='text-swing'>C</div>
-              <div id='text-swing-second' className='text-swing'>O</div>
-              <div id='text-swing-third' className='text-swing'>M</div>
+              <div id='dot-object' className={ count > 115 ? 'invisible animate' : animate ? 'animate' : ''}></div>
+              <div id='text-swing-first' className={count > 117 ? 'invisible text-swing' : 'text-swing'}>C</div>
+              <div id='text-swing-second' className={count > 119 ? 'invisible text-swing' : 'text-swing'}>O</div>
+              <div id='text-swing-third' className={count > 121 ? 'invisible text-swing' : 'text-swing'}>M</div>
+              <div id='pacman'>
+                <div id="mouth"></div>
+              </div>
             </div>)}
         }
         </BrowserOnly>
